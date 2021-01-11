e80f12cfe22ebd462dbf0b2dbd2e0cf66b591b59
Leo Kuo <p900372012@gmail.com>
2021-01-11 22:32:45 +0800

    Initial Commit

---
+++ messaging-app-swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  messaging-app-swiftUI
+//
+//  Created by 郭家銘 on 2021/1/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ messaging-app-swiftUI/messaging_app_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  messaging_app_swiftUIApp.swift
+//  messaging-app-swiftUI
+//
+//  Created by 郭家銘 on 2021/1/11.
+//
+
+import SwiftUI
+
+@main
+struct messaging_app_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
